7aca85a6b50dea5df2b283ba894596acb9650db6
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-06-06 23:28:43 -0500

    Version 1.000000000001 is live!

--- index.js
+++ index.js
@@ -1,26 +1,81 @@
-import { TodoistApi } from '@doist/todoist-api-typescript'
-import { configDotenv,  } from 'dotenv'
+import { TodoistApi } from "@doist/todoist-api-typescript";
+import { configDotenv } from "dotenv";
+import inquirer from "inquirer";
+
 configDotenv();
+console.clear();
 
-console.clear()
+const API_KEY = process.env.TODOIST_API_KEY;
+const todoist = new TodoistApi(API_KEY);
 
 
-const API_KEY = process.env.TODOIST_API_KEY
 
-const api = new TodoistApi(API_KEY)
 
-// api.getTasks({label: "task master"})
-//     .then((tasks) => {
-//         tasks.results.map((task) => {
-//             console.log(task.due)
-//         })
-//     })
-//     .catch((error) => console.log(error))
+const scheduleToday = async () => {
+	try {
+		let allLabelTasks = await todoist
+			.getTasksByFilter({ query: "@taskmaster & today" })
+			.then((data) => data.results);
 
-// api.getLabels()
-//     .then((data) => console.log(data))
-//     .catch((error) => console.log(error))
+            if (allLabelTasks.length < 1) {
+                console.log('There are not any tasks available.');
+                console.log(' ');
+            }
 
-api.getTasksByFilter({query: 'today|overdue'})
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
+		for (const task of allLabelTasks) {
+			await todoist.closeTask(task.id);
+
+			const answer = await inquirer.prompt([
+				{
+					type: "input",
+					name: "newTask",
+					message: `How would you like to use the time block: ${task.content}?`,
+				},
+			]);
+
+			await todoist.addTask({
+				content: answer.newTask,
+				dueDate: task.due.date,
+				duration: task.duration.amount,
+				durationUnit: task.duration.unit,
+			});
+		}
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+
+
+
+const runScript = async () => {
+	const answer = await inquirer.prompt([
+		{
+			// type: "select",
+            type: "list",
+			name: "pickTool",
+			message: "Choose and option...\n\n",
+			choices: ["Schedule Today", "Exit"],
+			default: "Exit",
+		},
+	]);
+	await console.log(answer.pickTool);
+
+	switch (answer.pickTool) {
+		case "Schedule Today":
+			console.log(" ");
+			await scheduleToday();
+			break;
+		case "Exit":
+			console.clear();
+            console.log('Have a nice day!');
+			console.log(" ");
+			process.exit(0);
+	}
+
+	await runScript();
+};
+
+
+
+runScript();
--- readme.md
+++ readme.md
@@ -3,14 +3,16 @@
 ## Set up and configuration
  - download the repository
  - create a file at the route of the directory named .env (I'm going to make a script for this...)
+ - paste this into your newly created file and follow the comments for more details
 
-
- ``` ini
+ ```ini
 # Replace the values with your API keys
 # just delete the entire line for any you don't want to use.
 TODOIST_API_KEY = "00000000000000000000000000000000000000"
 
 # note: do not delete the .gitignore file or you might leak your API keys
  ```
+
+- run the command ```taskmaster``` and you are will be up and running!
 
 ## Commands
